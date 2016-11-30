9f81d50f37b594d5a5ef970f6f3259ae32d4fc8b
ECHO <yangzhouyuan@pangzi.com>
2016-11-30 16:40:44 +0800

    新建文件

--- flow_main.cpp
+++ flow_main.cpp
@@ -1,13 +1,22 @@
 ﻿#include "flow_main.h"
 #include "ui_flow_main.h"
 #include <QDebug>
+#include "canvas_body.h"
+#include <QAction>
 
 flow_main::flow_main(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::flow_main)
 {
     ui->setupUi(this);
+    mdi_area_->setHorizontalScrollBarPolicy(Qt::ScrollBarAsNeeded);
+    mdi_area_->setVerticalScrollBarPolicy(Qt::ScrollBarAsNeeded);
+    setCentralWidget(mdi_area_);
+
     connect (drawer_.get (), &QDockWidget::visibilityChanged, [this] (bool){ on_drawer_visibility_changed (); });
+    connect(ui->action_file_new, SIGNAL(triggered()), this, SLOT(file_new()));
+
+
 }
 
 void flow_main::set_drawer(const QStringList& data)
@@ -28,6 +37,22 @@ void flow_main::set_drawer(const QStringList& data)
 flow_main::~flow_main()
 {
     delete ui;
+}
+
+void flow_main::file_new()
+{
+    canvas_body *canvs = create_canvas_body();
+    qDebug () << __func__;
+    canvs->show();
+
+}
+
+canvas_body *flow_main::create_canvas_body()
+{
+    canvas_body* canvas = new canvas_body();
+    mdi_area_->addSubWindow(canvas);
+    canvas->setWindowState(Qt::WindowMaximized);//使得创建的新窗口默认为最大化
+    return canvas;
 }
 
 
--- flow_main.h
+++ flow_main.h
@@ -4,7 +4,9 @@
 #include <QMainWindow>
 #include <QDockWidget>
 #include <memory>
+#include <QMdiArea>
 #include "drawer_list.h"
+#include "canvas_body.h"
 
 namespace Ui {
 class flow_main;
@@ -19,15 +21,21 @@ public:
     void set_drawer (const QStringList& data);
     ~flow_main();
 
+
 private slots:
     void on_drawer_visibility_changed ();
 
     void on_action_drawer_triggered();
+    ///新建文件
+    void file_new();
+    canvas_body *create_canvas_body();
 
 private:
     Ui::flow_main *ui;
     std::unique_ptr<QDockWidget> drawer_ = std::make_unique<QDockWidget> (this);
     std::unique_ptr<drawer_list> drawer_content_ = std::make_unique<drawer_list> (drawer_.get ());
+
+    QMdiArea *mdi_area_ = new QMdiArea (this);//是一个多文档显示窗口
 };
 
 #endif // FLOW_MAIN_H
