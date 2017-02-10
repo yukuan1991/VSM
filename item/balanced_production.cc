403163c0dbec51ff20ccfacecd823764467a26b2
ECHO <yangzhouyuan@pangzi.com>
2017-02-10 11:29:20 +0800

    modifted balanced and information

--- item/balanced_production.cc
+++ item/balanced_production.cc
@@ -26,6 +26,26 @@ void balanced_production::paint(QPainter *painter, const QStyleOptionGraphicsIte
 {
     Q_UNUSED(widget);
 
+//    auto the_pen = painter->pen ();
+//    the_pen.setColor(Qt::black);
+//    the_pen.setWidthF(0.02 * item_width_);
+//    painter->setPen(the_pen);
+
+//    QRectF rect (0.05 * item_width_, 0.25 * item_height_, 0.9 * item_width_, 0.5 * item_height_);
+
+//    auto font = painter->font();
+//    font.setBold(true);
+//    font.setPointSize (12);
+//    QFontMetricsF metrics (font);
+
+//    auto w = metrics.width("O X O X");
+//    auto h = metrics.height();
+
+//    auto center = rect.center();
+//    auto text_rect = QRectF (center.x() - 0.5 * w, center.y() - 0.5 * h, w, h);
+
+//    painter->drawRect(rect);
+//    painter->drawText(text_rect,"O X O X", Qt::AlignVCenter | Qt::AlignCenter);
     auto the_pen = painter->pen ();
     the_pen.setColor(Qt::black);
     the_pen.setWidthF(0.02 * item_width_);
@@ -33,16 +53,22 @@ void balanced_production::paint(QPainter *painter, const QStyleOptionGraphicsIte
 
     QRectF rect (0.05 * item_width_, 0.25 * item_height_, 0.9 * item_width_, 0.5 * item_height_);
 
-
-    QFontMetricsF metrics (painter->font());
+    auto font = painter->font();
+    font.setBold(true);
+    font.setPointSize (12);
+    QFontMetricsF metrics (font);
     auto w = metrics.width("O X O X");
     auto h = metrics.height();
 
     auto center = rect.center();
     auto text_rect = QRectF (center.x() - 0.5 * w, center.y() - 0.5 * h, w, h);
 
+    painter->setBrush(Qt::white);
     painter->drawRect(rect);
+
+    painter->setFont(font);
     painter->drawText(text_rect,"O X O X", Qt::AlignVCenter | Qt::AlignCenter);
+
     item::paint(painter, option, widget);
 }
 }
